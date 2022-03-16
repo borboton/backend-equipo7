d8fa05eba87a9440f093878e5dbfe7a805d370db
Raul Tedesco <raul.a.tedesco@gmail.com>
2022-03-16 16:10:50 -0300

    boilerplate db + config

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const logger = require('./config/logger');
+const config = require('./config/config');
+const dbConnection = require('./config/dbConnection');
 
 const app = express();
+
+// mongoose.connect(config.mongoose.url, {
+//   useNewUrlParser: true,
+//   useFindAndModify: false,
+//   useUnifiedTopology: true,
+// });
+dbConnection(config.mongoose.url);
 
 app.use(cors());
 app.options('*', cors());
