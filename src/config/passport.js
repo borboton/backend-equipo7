fc769efebbfd3d575bde6355f2fa97945f52a946
Raul Tedesco <raul.a.tedesco@gmail.com>
2022-03-17 17:18:35 -0300

    passport login feature

---
+++ src/config/passport.js
@@ -0,0 +1,67 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const logger = require('./logger');
+const User = require('../models/user.schema');
+
+// Signup
+
+passport.use(
+  'signup',
+  new LocalStrategy(
+    {
+      usernameField: 'userFile',
+      passwordField: 'password',
+      passReqToCallback: true,
+    },
+    async (req, email, password, done) => {
+      try {
+        const { name, lastname, userFile, avatar, phone, maritalStatus } =
+          req.body;
+        logger.info(req.body);
+        const user = await User.create({
+          email,
+          password,
+          name,
+          lastname,
+          userFile,
+          avatar,
+          phone,
+          maritalStatus,
+        });
+
+        return done(null, user);
+      } catch (e) {
+        return done(e);
+      }
+    }
+  )
+);
+
+// Login
+passport.use(
+  'signin',
+  new LocalStrategy(
+    {
+      usernameField: 'userFile',
+      passwordField: 'password',
+    },
+    async (userFile, password, done) => {
+      logger.info(`auth file --> login:userFile:${userFile}`);
+      logger.info(`auth file --> login:password:${password}`);
+      try {
+        const user = await User.findOne({ userFile });
+        logger.info(`auth file --> login:user:model:${user}`);
+        if (user) {
+          const validate = await user.isValidPassword(password);
+          if (!validate) {
+            return done(null, false, { message: 'Wrong password' });
+          }
+          return done(null, user, { message: 'Login successfull' });
+        }
+        return done(null, false, { message: 'User not found' });
+      } catch (e) {
+        return done(e);
+      }
+    }
+  )
+);
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-
+const passport = require('passport');
+const logger = require('../config/logger');
 const {
   getUsersController,
   postUserController,
-} = require('../controllers/users.controller');
+} = require('../controllers/user.controller');
 
 const usersRouter = express.Router();
 
@@ -13,5 +14,29 @@ usersRouter.get('/', getUsersController);
 // usersRouter.get('/:id/courses', httpGetUserCourses);
 // usersRouter.get('/:id/software', httpGetSoftwareAccess);
 usersRouter.post('/', postUserController);
+// Login with passport
+usersRouter.post('/signin', async (req, res, next) => {
+  passport.authenticate('signin', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        logger.info(err);
+        return res.status(403).json(info);
+      }
+      return res.json(user);
+    } catch (e) {
+      return next(e);
+    }
+  })(req, res, next);
+});
+usersRouter.post(
+  '/signup',
+  passport.authenticate('signup', { session: false }),
+  async (req, res) => {
+    res.json({
+      message: 'Signup successful',
+      user: req.user,
+    });
+  }
+);
 
 module.exports = usersRouter;
