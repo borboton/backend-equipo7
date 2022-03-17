01a797d3272090667b1da5a54ff04860661099be
Sergio Villavicencio <sergionicolasvillavicencio@gmail.com>
2022-03-17 20:51:30 -0300

    fix: folder structure 2

---
+++ src/middleware/passport.middleware.js
@@ -0,0 +1,31 @@
+const passport = require('passport');
+const logger = require('../config/logger');
+
+async function signIn(req, res, next) {
+  passport.authenticate('signin', async (err, user, info) => {
+    try {
+      if (err || !user) {
+        logger.info(err);
+        return res.status(403).json(info);
+      }
+      return res.json(user);
+    } catch (e) {
+      return next(e);
+    }
+  })(req, res, next);
+}
+
+const passportSignUp = passport.authenticate('signup', { session: false });
+
+async function signUp(req, res) {
+  res.json({
+    message: 'Signup successful',
+    user: req.user,
+  });
+}
+
+module.exports = {
+  signIn,
+  signUp,
+  passportSignUp,
+};
